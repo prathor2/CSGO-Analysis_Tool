59a43dfc3afad91dc14159ebe2c716aae93923b2
prathor2 <123683540+prathor2@users.noreply.github.com>
2023-05-24 19:57:03 -0400

    Update process_game_state.py

--- process_game_state.py
+++ process_game_state.py
@@ -6,25 +6,36 @@ from shapely.geometry import Polygon, Point
 
 class ProcessGameState:
     def __init__(self, parquet_file, boundary_vertices):
-        # Loading data from parquet file
-        self.data = pd.read_parquet(parquet_file)
-
-        # Creating a polygon from boundary vertices
-        self.polygon = Polygon(boundary_vertices)
+        """
+        Initialize the class with game data and boundary vertices
+        :param parquet_file: The game data file in Parquet format
+        :param boundary_vertices: The vertices of the polygonal boundary
+        """
+        self.data = pd.read_parquet(parquet_file)  # Load data
+        self.polygon = Polygon(boundary_vertices)  # Create polygon
+        # Run initial data processing
+        self.filter_rows_in_boundary()
+        self.extract_weapon_classes()
 
     def filter_rows_in_boundary(self):
-        """Assigns True or False to each row depending on whether the point is within the boundary"""
+        """
+        Assign True or False to each row depending on whether the point is within the boundary and Z-Axis Bounds
+        """
         xy_coords = list(zip(self.data['x'], self.data['y']))
-        self.data['in_boundary'] = [self.polygon.contains(Point(point)) for point in xy_coords]
+        self.data['in_boundary'] = [self.polygon.contains(Point(point)) for point in xy_coords] & (self.data['Z'] >= 285) & (self.data['Z'] <= 421)
 
     def extract_weapon_classes(self):
-        """Extracts weapon classes from the inventory column and adds it as a new column"""
-        # Check if 'inventory' column is in DataFrame
+        """
+        Extracts weapon classes from the inventory column and adds it as a new column
+        Raises ValueError if the 'inventory' column is not in the DataFrame
+        """
         if 'inventory' not in self.data.columns:
             raise ValueError("Missing 'inventory' column in DataFrame")
 
         def extract_classes(inventory_json):
-            """Extracts classes from inventory json"""
+            """
+            Helper function to extract classes from inventory json
+            """
             try:
                 return [item['class'] for item in json.loads(inventory_json).values()]
             except (json.JSONDecodeError, KeyError):
@@ -33,32 +44,40 @@ class ProcessGameState:
         self.data['weapon_classes'] = self.data['inventory'].apply(extract_classes)
 
     def team_strategy(self, team, side):
-        """Calculates the mean of in_boundary values for the given team and side"""
+        """
+        Calculates the mean of in_boundary values for the given team and side
+        :param team: The team to analyze
+        :param side: The side (T or CT) to analyze
+        """
         team_data = self.data[(self.data['team'] == team) & (self.data['side'] == side)]
-
-        # Check if DataFrame is empty
         if team_data.empty:
             return "No data available for the given team and side"
 
         return team_data['in_boundary'].mean()
 
     def average_timer_with_weapons(self, team, side, min_weapons, weapon_types):
-        """Calculates the average timer for the given team and side with at least min_weapons of weapon_types"""
+        """
+        Calculates the average timer for the given team and side with at least min_weapons of weapon_types
+        :param team: The team to analyze
+        :param side: The side (T or CT) to analyze
+        :param min_weapons: The minimum number of specified weapon types
+        :param weapon_types: The types of weapons to consider
+        """
         team_data = self.data[(self.data['team'] == team) & (self.data['side'] == side)]
         team_data['weapon_count'] = team_data['weapon_classes'].apply(lambda x: sum([weapon in x for weapon in weapon_types]))
         filtered_data = team_data[team_data['weapon_count'] >= min_weapons]
-
-        # Check if DataFrame is empty
         if filtered_data.empty:
             return "No data available for the given conditions"
 
         return filtered_data['seconds'].mean()
 
     def heatmap_coordinates(self, team, side):
-        """Generates a heatmap of coordinates for the given team and side"""
+        """
+        Generates a heatmap of coordinates for the given team and side
+        :param team: The team to analyze
+        :param side: The side (T or CT) to analyze
+        """
         team_data = self.data[(self.data['team'] == team) & (self.data['side'] == side) & (self.data['in_boundary'])]
-        
-        # Check if DataFrame is empty
         if team_data.empty:
             return "No data available for the given team and side"
 
